23451f86bb70021949185e33f22f57ba9ae67f1c
Florian WATTIER <fwattier@live.fr>
2019-07-18 08:16:29 +0200

    send things

---
+++ app.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+const routes = require('./api');
+const mongoose = require('./config/mongoose');
+const model = require('./app.model');
+
+app.use(cors());
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+model.initialize();
+mongoose.connect();
+app.use(routes);
+
+// Catch 404 and forward to error handler
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+module.exports = app;
---
+++ config/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  mongo: {
+    uri: 'mongodb+srv://admin:mouton@biosain-voja5.mongodb.net/',
+    db: 'biosain'
+  },
+  api: 'http://localhost:3000/api'
+};
