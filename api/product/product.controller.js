2d4c23429040ffb716464b2ae65a268a67ab976c
Florian <fwattier@live.fr>
2020-09-29 12:07:58 +0200

    test

--- api/product/product.controller.js
+++ api/product/product.controller.js
@@ -7,7 +7,7 @@ const cheerio = require('cheerio');
 exports.findAll = async (req, res, next) => {
   try {
     const page = parseInt(req.query.page, 10) || 1;
-    const pagesize = parseInt(req.query.pagesize) || 170;
+    const pagesize = parseInt(req.query.pagesize) || 90;
     const month = req.query.month
     let filters = {}
     if (month) {
