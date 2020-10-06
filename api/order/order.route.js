c64a9208e13470182b07539c82014bbbbec38200
Florian WATTIER <fwattier@live.fr>
2020-10-06 23:30:25 +0200

    test

--- api/order/order.route.js
+++ api/order/order.route.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const orderController = require('./order.controller');
+const bodyParser = require('body-parser');
 
 router.route('/').get(orderController.findAll);
 router.route('/:id').patch(orderController.update);
 router.route('/:id').get(orderController.findById);
 router.route('/delivery/approval').post(orderController.isDeliveryPossible);
-router.route('/validate').post(orderController.validate);
+router.route('/validate', bodyParser.raw({type: 'application/json'})).post(orderController.validate);
 
 module.exports = router;
